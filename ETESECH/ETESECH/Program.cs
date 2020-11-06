63e805480f05e3a50e1fab21dc3eb34fb8980d9e
Pelucasss <srpelucassape@gmail.com>
2020-11-06 13:55:40 +0100

    Arregladow

--- ETESECH/ETESECH/Program.cs
+++ ETESECH/ETESECH/Program.cs
@@ -6,15 +6,11 @@ namespace ETESECH
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hola mundo soy Vanesa lhsdjdchsjhou");
+            Console.WriteLine("Hola mundo soy Vanesa");
             Console.WriteLine("¿Que tal estas Vanesa?");
-<<<<<<< HEAD
+
             Console.WriteLine("Muy bien, sigo viva por ahora");
-            Console.WriteLine("Alubu lula lulibi :3")
-=======
-            Console.WriteLine("Muy bien, sigo viva");
             Console.WriteLine("Alubu lula lulibi :3");
->>>>>>> d16df0637fa17ec9cd04cdc0a2eb72c565bc66d7
         }
     }
 }
