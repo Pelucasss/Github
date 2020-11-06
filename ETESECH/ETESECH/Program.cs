1cd57af3d4169da7cf00a9ab86d8ca7d27f24d75
Pelucasss <srpelucassape@gmail.com>
2020-11-06 13:44:44 +0100

    Figo

--- ETESECH/ETESECH/Program.cs
+++ ETESECH/ETESECH/Program.cs
@@ -6,7 +6,7 @@ namespace ETESECH
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hola mundo soy Vanesa");
+            Console.WriteLine("Hola mundo soy Vanesa prima");
             Console.WriteLine("¿Que tal estas Vanesa?");
             Console.WriteLine("Muy bien, sigo viva");
             Console.WriteLine("Alubu lula lulibi :3")
