5f6cf369daca144bff4962160d42844223af5179
Ivancuarius <ivancuarius@gmail.com>
2020-11-06 13:19:37 +0100

    comit

--- ETESECH/ETESECH/Program.cs
+++ ETESECH/ETESECH/Program.cs
@@ -6,7 +6,7 @@ namespace ETESECH
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hola");
+            Console.WriteLine("Hola mundo soy vanesa");
         }
     }
 }
